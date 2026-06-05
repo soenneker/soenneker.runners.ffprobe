4aee461d939526c2de5d4ed7abaa9a0667761292
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:26 -0500

    xml commenting

--- src/Soenneker.Runners.ffprobe/Constants.cs
+++ src/Soenneker.Runners.ffprobe/Constants.cs
@@ -1,7 +1,16 @@
 ﻿namespace Soenneker.Runners.ffprobe;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The file name.
+    /// </summary>
     public const string FileName = "ffprobe.exe";
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Libraries.ffprobe";
 }
